20ebd6ccf954b580abb895688eab28da38ad6430
mauricio <mamv.2137@gmail.com>
2019-12-04 00:03:07 -0300

    Only skeleton of the game with some color test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">
+        <h4>Simon Game</h4>
+      </div>
+      <div className="game">
+        {
+          ['green', 'red', 'yellow', 'blue'].map((item, index) => {
+            return <div key={index} className={`tiles tile--${item}`}>{index}</div>
+          })
+        }
+      </div>
     </div>
   );
 }
